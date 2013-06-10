a41be9f56681bb5cef2fa34e59d5cb060475bb97
qdx <qdxzzz@gmail.com>
2013-06-09 18:23:50 -0700

    finished question asking and anwser ajax. next would be solutions list display.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
+    // bind to tags to dynamically remove tag
     $("body").on( "click",".TagButton", function(){
         $(this).remove();
     } );
+
+    // bind to questions to dynamically ignore and remove question
     $("body").on( "click",".ignore_question", function(){
         $(this).parent().parent().attr("style","display:none");
     } );
+
+    // rate up a solution
     $("body").on( "click",".arrowUp", function(){
         //TODO: change the color of the arrow, forbid it from 
         // changing the rate anymore if it has been clicked once
@@ -12,85 +17,121 @@ $(document).ready(function(){
         var rate = $(this).parent().find(".SolutionRate").html();
         $(this).parent().find(".SolutionRate").html(++rate);
     } );
+    // rate down a solution
     $("body").on( "click",".arrowDown", function(){
         var rate = $(this).parent().find(".SolutionRate").html();
         $(this).parent().find(".SolutionRate").html(--rate);
     } );
 
-
+    // demo data
     var countries = [
     { value: 'Andorra', data: 'AD' },
     { value: 'Zimbabwe', data: 'ZZ' }
     ];
 
+    // bind a third-party autocomplete plugin to the search input box
     a = $('#SearchBoxInput').autocomplete({
         //TODO: For demo purpose:
         //lookup:countries,
-      //TODO: Uncomment the following line, replace it with the actual
-      // python script which answers autocomplete
-      serviceUrl:'SomeFolder/SomeService/autocomplete.py',
+
+        //TODO: Uncomment the following line, replace it with the actual
+        // python script which answers autocomplete <>
+        serviceUrl:'SomeFolder/SomeService/autocomplete.py',
       onSelect: function(suggestion){
           //alert($('#SearchBoxInput').val());
       }
     });
 });
 
+// called when user type in the search input box,
+// executed when the key is <Enter>. auto-completion
+// already handled by a third-party plugin mentioned above
 function ProcessKeyPress(e){
+    // when the key is enter
     if (e.keyCode == 13) {
 
+        // for demo purpose
         var testJson = "\
         {\
-            \"Questions\": [\
-            {\
-                \"id\": 1,\
-                    \"type\": 2,\
-                    \"data\": \"ubuntu\",\
-                    \"pid\": \"some id\"\
-            },\
+            \"Type\":\"question\",\
+                \"Questions\": [\
+                {\
+                    \"id\": 1,\
+                        \"type\": 2,\
+                        \"data\": \"ubuntu\",\
+                        \"pid\": \"some id\"\
+                },\
             {\
                 \"id\": 2,\
-                \"type\": 1,\
-                \"data\": \"OS\",\
-                \"pid\": \"some id\"\
+                    \"type\": 1,\
+                    \"data\": \"OS\",\
+                    \"pid\": \"some id\"\
             }\
             ]\
         }\
         "
-        var testJson = $.parseJSON(testJson);
-        HandleQueryResponse(testJson);
-        
-            // ajax for the initial query
-            $.ajax({
-                type:   'get',
-            url:    "SomePlace.py?" + $("#SearchBoxInput").html(),
+            //for demo
+            var testJson = $.parseJSON(testJson);
+
+        // ajax for the initial query
+        $.ajax({
+            type:   'get',
+            // put the python url which accept initial query here
+            // the query is simply put in the url to form a get request
+            url:    "HandleInitialQuery.py?" + $("#SearchBoxInput").val(),
             success:function(result){
                 var response = $.parseJSON(result);
+                // handle the question returned from server
+                HandleQueryResponse(testJson);
             },
             error:  function(){
                     }
-            });
+        });
     }
 }
 
-function SendGetRequest(params){
+// dynamically change the page, add all the questions input box
+// according the the response
+function HandleQueryResponse(response){
+    if(response["Type"] == "question"){
+        console.log("question list coming!");
+        HandleQuestionResponse(response);
+    }
+    else if(response["Type"] == "solution_list"){
+        console.log("solution list coming!");
+        HandleSolutionResponse(response);
+    }
+    else{
+        console.log("Encountered unexpected response type!");
+    }
 }
 
-function HandleQueryResponse(response){
+// handle the solution list response from the server, display the dynamically
+function HandleSolutionResponse(response){
+//TODO: implement
+}
+
+// handle the questions asked by the server, display the dynamically
+function HandleQuestionResponse(response){
     var qType = {1:"OS",2:"software"};
     $("#MainPageQuestion").attr("style","");
     //alert(response["Questions"][0]["data"]);
     $(response["Questions"]).each(function(index,val){
-        alert(val["id"]);
-        var questionString = "Please give the "+qType[val["type"]]+" version of "+val["data"];
-        var questionDiv = $("div.hidden_question").clone();
-        $(questionDiv).find("label").html(questionString);
-        $(questionDiv).find("input").attr("id",val["id"]);
-        $(questionDiv).attr("style","");
-        $(questionDiv).removeClass("hidden_question");
-        $("div.hidden_question").after(questionDiv);
+        //alert(val["id"]);
+        var questionString = "Please give the " +qType[val["type"]]
+        +" version of "+val["data"];
+    var questionDiv = $("div.hidden_question").clone();
+    $(questionDiv).find("label").html(questionString);
+    $(questionDiv).find("input").attr("id",val["id"]);
+    $(questionDiv).attr("style","");
+    $(questionDiv).addClass("answer_input");
+    $(questionDiv).removeClass("hidden_question");
+    $("div.hidden_question").after(questionDiv);
     });
 }
 
+// dynamically add tags, but it seems like the back-end does not
+// need this information for now, so did not send it in the request
 function AddTagFunction(){
     var newTagName = $("#TagInput").val();
     if(newTagName !== null && newTagName !== ""){
@@ -99,11 +140,51 @@ function AddTagFunction(){
         $(hiddenTag).attr("id","hiddenTagProcessing");
         $(hiddenTag).html(newTagName+"<i class=\"icon-remove\"></i>");
         $("#TagListSpan").append(hiddenTag);
+        $("#TagInput").val("");
     }
 }
 
-function HandleSubmit(){
+function HandleSubmit(type){
+    var params = new Array();
+    $(".answer_input input").each(function(index,val){
+        var id = $(this).attr("id");
+        var answer = $(this).val();
+        // if the user choose to ignore all the questions,
+        // this code will generate an empty answer for each
+        // question
+        if(type == "ignore_all"){
+            console.log("ignore");
+            params.push({"id":id,"answer":""});
+        }
+        else{
+            params.push({"id":id,"answer":answer});
+        }
+    });
+
+    // remove the previous questions
+    $(".answer_input").each(function(){
+        $(this).remove();
+    });
+    // hide the question asking bar
+    $("#MainPageQuestion").attr("style","display:none;");
+
+    // parse the answers into json
+    var answer = {"Answers":params};
+    var json_answer = JSON.stringify(answer);
+    console.log(json_answer);
+    // send the answers as json
+    $.ajax({
+        type:   'post',
+        url:    "HandleAnswer.py?" + $("#SearchBoxInput").html(),
+        // I can't see the actual payload data from my debug tools, I assume it
+        // do send the json, if it does not work correctly, please let me know
+        data:   json_answer,
+        success:function(result){
+            var response = $.parseJSON(result);
+            HandleQueryResponse(result);
+        },
+        error:  function(){
+                }
+    });
 }
 
-function IgnoreAll(){
-}
